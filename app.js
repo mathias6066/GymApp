4bd7af587681848175be7cfe2b5272f1cb246b3d
mathias6066 <42849528+mathias6066@users.noreply.github.com>
2021-08-06 19:24:07 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+$(' #modaltable ').on('click',function(){
+    $("#myModal").modal("show");
+});
+
+$(' #modaltable1 ').on('click',function(){
+    $("#myModal1").modal("show");
+});
+
+$(' #modaltable2 ').on('click',function(){
+    $("#myModal2").modal("show");
+});
+
+$(' #modaltable3 ').on('click',function(){
+    $("#myModal3").modal("show");
+});
+
+$(' #modaltable4 ').on('click',function(){
+    $("#myModal4").modal("show");
+});
+
+$(' #modaltable5 ').on('click',function(){
+    $("#myModal5").modal("show");
+});
